3364ffcaddbbc003863a2140aea4aa58968b609a
André Leão <andrefelipesl123@gmail.com>
2025-03-07 11:47:22 -0300

    transformStateWithClones function

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,32 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  let newState = { ...state };
+
+  actions.forEach((action) => {
+    switch (action.type) {
+      case 'addProperties':
+        newState = { ...newState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach((key) => {
+          const { [key]: _, ...rest } = newState;
+
+          newState = rest;
+        });
+        break;
+
+      case 'clear':
+        newState = {};
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  return [newState];
 }
 
 module.exports = transformStateWithClones;
